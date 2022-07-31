5381a85cef9f0fa1d17328e9d06bfdd4d8f0ada5
midhunps <midhunps.0@gmail.com>
2022-07-31 13:43:49 +0530

    Generated app.js, app.css with production version

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,10 +19,10 @@ currentroute=$event.detail.currentroute;"
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
         <!-- Styles -->
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
         <!-- Scripts -->
-        <script src="{{ asset('js/app.js') }}" defer></script>
+        <script src="{{ mix('js/app.js') }}" defer></script>
     </head>
     <body x-data="initPage" x-init="initAction();"
         @linkaction.window="initialised = false; fetchLink($event.detail);"
