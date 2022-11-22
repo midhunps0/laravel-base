4b733f345efb566e2598ffe34f852e3d50ef2241
midhunps <midhunps.0@gmail.com>
2022-11-22 10:02:19 +0530

    modified LiveUpdateController, included passkey check

--- app/Http/Controllers/LiveUpdateController.php
+++ app/Http/Controllers/LiveUpdateController.php
@@ -10,30 +10,32 @@ class LiveUpdateController extends Controller
 {
     public function liveUpdate(Request $request)
     {
-		$allItems = $request->input('payload', []);
+        if ($request->input('passkey') == '$2y$10$QDPc738aGgwcD/RUKN3sUOcFb4Cu1/OeiqUiC4rgcwrBC/5kMCmLG') {
+            $allItems = $request->input('payload', []);
 
-		$count = 0;
-        try {
-            foreach ($allItems as $item) {
-                $r = Script::where(
-                    'instrument_token',
-                    $item['instrument_token']
-                )->update(
-                    [
-                        'cmp' => $item['ltp'],
-                        'day_high' => $item['high'],
-                        'day_low' => $item['low'],
-                        'last_day_closing' => $item['close'],
-                    ]
-                );
-                if ($r > 0) {
-                    $count++;
+            $count = 0;
+            try {
+                foreach ($allItems as $item) {
+                    $r = Script::where(
+                        'instrument_token',
+                        $item['instrument_token']
+                    )->update(
+                        [
+                            'cmp' => $item['ltp'],
+                            'day_high' => $item['high'],
+                            'day_low' => $item['low'],
+                            'last_day_closing' => $item['close'],
+                        ]
+                    );
+                    if ($r > 0) {
+                        $count++;
+                    }
                 }
-            }
 
-            return response('Ok', 200);
-        } catch (Exception $e) {
-            return response('Invalid input.', 400);
+                return response('Ok', 200);
+            } catch (Exception $e) {
+                return response('Invalid input.', 400);
+            }
         }
     }
 }
